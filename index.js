3571985b5c5fe8f55f7b332ce99d8df0fea487e1
Katie Blizzard <kblizzard11@gmail.com>
2019-01-08 00:13:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* code goes here */
 document.addEventListener("DOMContentLoaded", e => {
-const hiddendiv = document.getElementById("hiiden-div");
+const hiddendiv = document.getElementById("hidden-div");
 hiddendiv.innerHTML("301");
 })
