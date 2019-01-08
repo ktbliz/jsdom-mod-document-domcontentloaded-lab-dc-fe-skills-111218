394a711651a80732d14da1af7b406be14a507ca1
Katie Blizzard <kblizzard11@gmail.com>
2019-01-08 00:01:08 +0000

    Automatically backed up by Learn

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-document.addEventListener("DOMContentLoaded", e => ( 
-  document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"))}
+document.addEventListener("DOMContentLoaded", e => { 
+  document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
+})
